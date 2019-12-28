648c9addb1457a67c21d391631ffdd8ed1dbe4cf
AzaniaBG <azaniazbaker@gmail.com>
2019-12-28 12:46:46 -0800

    second commit

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 //define a variable in which the base url will be stored
 const baseURL = `https://api.github.com/search/users`;
-const reposURL = `https://api.github.com/users/:username/repos`;
 //function takes one argument, an object, which is formatted to comply w/ API requirements
 function formatQueryParameters(params) {
     let queryItems = Object.keys(params).map(key => `${key}=${params[key]}`);
@@ -24,21 +23,24 @@ console.log(`url is ${url}`);
             return response.json();
         }
         throw new Error(response.statusText);
-        }).then(responseJson => {
-console.log(responseJson)
-            let userItems = responseJson.items[0].login;
-            let userRepos = responseJson.items[0].repos_url;
-            $("#js-user-results").append(`<li>${userItems}
-            <a href=${userRepos}>User Repo</a></li>`);
-            
-console.log(`userItems is ${userItems}`);
-console.log(`userRepos is ${userRepos}`);
-
-        }).catch(err => {
-            $("#js-error-message").text(`Oopsie poopsie! ${err.message}`)
+        }).then(responseJson => displayResults(responseJson)).catch(err => {
+            $("#js-error-message").text(`Oopsie poopsie! ${err.message}`);
+            $("#error-message").removeClass("hidden");
 console.log(`err is ${err.message}`);
         });
-}
+};
+//function displays responses to page 
+function displayResults(responseJson) {
+    $("#results").empty;
+    for(let i = 0; i < responseJson.items.length; i++) {
+        let userItems = responseJson.items[i].login;
+        let userRepos = responseJson.items[i].repos_url;
+        $("#js-user-results").append(`<li>${userItems}: <a href="${userRepos}">${userRepos}</a></li>`)
+console.log(`userItems is ${userItems}`);
+console.log(`userRepos is ${userRepos}`);     
+    }
+    $("#results").removeClass("hidden");
+};
 //function watches for input submitted on form
 function watchForm() {
 //listen for the `submit` event and capture the user's input
